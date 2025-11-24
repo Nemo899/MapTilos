41d4939b286bc4638f7c934689929682ffecea39
Nemo899 <62214915+Nemo899@users.noreply.github.com>
2025-11-24 16:56:39 +0100

    Add files via upload

---
+++ data/ALBERI_AREATILOS_2.js
@@ -0,0 +1,8 @@
+var json_ALBERI_AREATILOS_2 = {
+"type": "FeatureCollection",
+"name": "ALBERI_AREATILOS_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "width": 5.0 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.33179782187996, 36.457961501453454 ] ] } }
+]
+}
